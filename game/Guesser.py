c6b8e3ff8e9931124cfc8e42f9fa063454d220c0
ederson192 <ederson192@gmail.com>
2022-02-09 11:58:01 -0500

    Final comments

--- game/Guesser.py
+++ game/Guesser.py
@@ -1,7 +1,24 @@
 import random
 class Guesser: 
+    """A person who guess the letter. 
+    
+    The responsibility of a Guesser is to receive the input letter and compare it with the 
+    random secrer word.
+
+    Attributes:
+        list (list): List of random words.
+        word (str): secret word 
+        _word_in_progress (list): Guessed letters 
+        j (int): loop index
+        size (int) = lenght of word
+    """
     
     def __init__(self):
+        """Constructs a new Guesser.
+        
+        Args:
+            self (Guesser): an instance of Guesser.
+        """
         
         self.list = ['stock', 'over', 'flow', 'random', 'hello', 'computer', 'happy', 'python', 'health', 'funny']
         self.word = random.choice(self.list)
@@ -13,6 +30,14 @@ class Guesser:
             self._word_in_progress += '_'
 
     def guess (self): 
+        """Gets the guess from the player and compare it with the secret word.
+
+        Args:
+            self (Guesser): An instance of Guesser.
+        
+        Returns:
+            list: word in progress.
+        """
         self.guesser = False
 
         guess = input('Guess a letter [a-z]: ')
@@ -25,8 +50,7 @@ class Guesser:
 
         return self._word_in_progress 
 
-    def is_found (self):
-        return (self._word_in_progress == self.word)
+   
 
 
 
